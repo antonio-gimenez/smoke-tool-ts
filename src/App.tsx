4081166985043fd3b98a2271847adb216186cd2c
Antonio Gimenez <grupocapgemini-services.antonio.gimenez@hp.com>
2023-02-06 19:32:22 +0100

    Cleaning 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header.jsx";
+import WhatsNew from "./components/WhatsNew.jsx";
+import { AlertProvider } from "./contexts/AlertContext.jsx";
+import { ModalProvider } from "./contexts/ModalContext.jsx";
+import { TestProvider } from "./contexts/TestContext.jsx";
+import useTheme from "./hooks/use-theme.jsx";
+import SmokeTest from "./pages/SmokeTest.jsx";
 
 function App() {
+  const { theme } = useTheme();
+  const html = document.querySelector("html");
+  if (html) {
+    html.setAttribute("data-theme", theme);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AlertProvider>
+      <ModalProvider>
+        {/* <TestProvider> */}
+        <Header />
+        <div className="app container scrollable-content container-centered">
+          <WhatsNew />
+          <Routes>
+            <Route path="/" element={<SmokeTest />} />
+          </Routes>
+        </div>
+        {/* </TestProvider> */}
+      </ModalProvider>
+    </AlertProvider>
   );
 }
 
