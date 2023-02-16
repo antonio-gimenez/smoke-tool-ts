4819c72076d2995cd6a0df98067e5fa5868cea78
Antonio Gimenez <58783747+antonio-gimenez@users.noreply.github.com>
2023-02-16 13:23:29 +0100

    work on colors

--- src/components/ThemeSelector/ThemeSelector.tsx
+++ src/components/ThemeSelector/ThemeSelector.tsx
@@ -50,11 +50,7 @@ function ThemeSelector({ themes }: ThemeSelectorProps): JSX.Element {
   };
 
   const Palette = ({ color }: { color: string }): JSX.Element => (
-    <span className={`theme-list-palette-${color}`}>
-      {/* show only the first letter of the color in uppercase */}
-      {/* {color[0].toUpperCase()} */}
-      A
-    </span>
+    <span className={`theme-list-palette-${color}-bullet`} />
 
   );
 
