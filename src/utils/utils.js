40f2ee52fa142365f018ff8d5358443105119c9a
Antonio Gimenez <angiru1991@gmail.com>
2023-02-23 17:03:37 +0100

    t2

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -108,63 +108,68 @@ export function typeIncludes(fileType, typeString) {
 }
 
 export async function sendReport(selectedItems) {
-  const files = await Promise.all(
-    selectedItems?.map(async (item) => {
-      if (item.files.length > 0) {
-        const response = await api.get(`/tests/files/${item._id}`);
-        return response.data.data;
-      }
-    })
-  );
+  try {
+    const files = await Promise.all(
+      selectedItems?.map(async (item) => {
+        if (item.files.length > 0) {
+          const response = await api.get(`/tests/files/${item._id}`);
+          return response.data.data;
+        }
+      })
+    );
 
-  const filteredFiles = files.flat().filter((file) => file);
+    const filteredFiles = files.flat().filter((file) => file);
 
-  console.log(filteredFiles);
+    const attachments = await Promise.all(
+      filteredFiles.map(async (file) => {
+        const data = btoa(String.fromCharCode(...file.file.data));
+        return {
+          filename: file.name,
+          data,
+        };
+      })
+    );
 
-  const attachments = await Promise.all(
-    filteredFiles.map(async (file) => {
-      const data = btoa(String.fromCharCode(...file.file.data));
-      return {
-        filename: file.name,
-        data,
-      };
-    })
-  );
+    const subject = "Email with attachments";
+    const body = "This is the body of the email.";
+    const from = "sender@example.com";
+    const to = "recipient@example.com";
 
-  const subject = "Email with attachments";
-  const body = "This is the body of the email.";
-  const from = "sender@example.com";
-  const to = "recipient@example.com";
+    const boundary = "my-multipart-boundary";
+    const multipartContent = [
+      `Content-Type: text/plain\r\n\r\n${body}\r\n`,
+      ...attachments.map(
+        (attachment) =>
+          `--${boundary}\r\n` +
+          `Content-Type: application/octet-stream\r\n` +
+          `Content-Disposition: attachment; filename="${attachment.filename}"\r\n` +
+          `Content-Transfer-Encoding: base64\r\n\r\n` +
+          `${attachment.data}\r\n`
+      ),
+      `--${boundary}--\r\n`,
+    ].join("");
 
-  const boundary = "my-multipart-boundary";
-  const multipartContent = [
-    `Content-Type: text/plain\r\n\r\n${body}\r\n`,
-    ...attachments.map(
-      (attachment) =>
-        `--${boundary}\r\n` +
-        `Content-Type: application/octet-stream\r\n` +
-        `Content-Disposition: attachment; filename="${attachment.filename}"\r\n` +
-        `Content-Transfer-Encoding: base64\r\n\r\n` +
-        `${attachment.data}\r\n`
-    ),
-    `--${boundary}--\r\n`,
-  ].join("");
+    const emailContent =
+      `From: ${from}\r\nTo: ${to}\r\nSubject: ${subject}\r\n` +
+      `Content-Type: multipart/mixed; boundary="${boundary}"\r\n\r\n${multipartContent}`;
 
-  const emailContent =
-    `From: ${from}\r\nTo: ${to}\r\nSubject: ${subject}\r\n` +
-    `Content-Type: multipart/mixed; boundary="${boundary}"\r\n\r\n${multipartContent}`;
+    const emlContent = `data:application/octet-stream;base64,${btoa(emailContent)}`;
 
-  const emlContent = `data:application/octet-stream;base64,${btoa(emailContent)}`;
+    var encodedUri = encodeURI(emlContent);
+    var a = document.createElement("a");
+    var linkText = document.createTextNode("fileLink");
+    a.appendChild(linkText);
+    a.href = encodedUri;
+    a.id = "fileLink";
+    a.download = "report.eml";
+    a.style = "display:none;";
+    document.body.appendChild(a);
+    document.getElementById("fileLink").click();
+    document.body.removeChild(a);
 
-  var encodedUri = encodeURI(emlContent);
-  var a = document.createElement("a");
-  var linkText = document.createTextNode("fileLink");
-  a.appendChild(linkText);
-  a.href = encodedUri;
-  a.id = "fileLink";
-  a.download = "report.eml";
-  a.style = "display:none;";
-  document.body.appendChild(a);
-  document.getElementById("fileLink").click();
-  document.body.removeChild(a);
+    return true; // indicate success
+  } catch (error) {
+    console.error("Error sending report:", error);
+    return false; // indicate failure
+  }
 }
